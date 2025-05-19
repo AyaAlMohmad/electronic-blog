d336144cdf10f1f1f69dafe4838f46868fa7521a
AyaAlMohmad <mhaya565@gmail.com>
2025-05-19 19:36:20 +0300

    part 6.1 in dashboard and api

--- app/Http/Middleware/CheckAdmin.php
+++ app/Http/Middleware/CheckAdmin.php
@@ -15,6 +15,10 @@ class CheckAdmin
      */
     public function handle(Request $request, Closure $next): Response
     {
+        if (!auth()->check() || !auth()->user()->is_admin) {
+            abort(403, 'Unauthorized');
+        }
+    
         return $next($request);
     }
 }
